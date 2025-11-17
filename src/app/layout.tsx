bbc146b5d95eb2699a54d8fa84db4cb346d9c402
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-11-17 00:07:05 -0800

    NextJS er font kivabe dewya jay sikhlam

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono, poppins } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const poppins = Poppins({
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: {
@@ -32,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <Navbar />
         <main className="min-h-[calc(100vh-320px)]">{children}</main>
