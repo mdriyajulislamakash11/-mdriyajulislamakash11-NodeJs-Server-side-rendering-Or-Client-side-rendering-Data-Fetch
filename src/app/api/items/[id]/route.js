3298f91c2f1952b1c981602c19fb9b739968a994
Rj Akash Chowdury <hafejmohammadakash@gmail.com>
2025-11-17 09:57:36 -0800

    api er kaj suru korlanm

---
+++ src/app/api/items/[id]/route.js
@@ -0,0 +1,27 @@
+
+
+// get dynamic 
+export async function GET(req, { params }) {
+  const p = await params;
+  console.log(p);
+
+  return Response.json({ params: p });
+}
+
+// Update 
+export async function PATCH(req, { params }) {
+  const p = await params;
+  console.log(p);
+
+  return Response.json({ params: p });
+}
+
+// Delete
+export async function DELETE(req, { params }) {
+  const p = await params;
+  console.log(p);
+
+  return Response.json({ params: p });
+}
+
+
---
+++ src/app/api/items/route.js
@@ -0,0 +1,18 @@
+
+
+
+export async function GET() {
+  const data = {
+    message: "successfully get data.",
+    error: false,
+    status: 200,
+  }
+ 
+  return Response.json({ data })
+}
+
+export async function POST (req) {
+    const postedData = await req.json()
+
+    return Response.json({postedData})
+}
